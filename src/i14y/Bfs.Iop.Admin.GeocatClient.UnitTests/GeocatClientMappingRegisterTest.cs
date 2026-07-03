43e2246fbd03aae766d4011bcc89270d2f485f92
Nuno Canadas <nuno.canadas@bfs.admin.ch>
2026-07-03 11:24:01 +0200

    adjust unit tests

---
+++ src/i14y/Bfs.Iop.Admin.GeocatClient.UnitTests/GeocatClientMappingRegisterTest.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using NUnit.Framework;
+
+namespace Bfs.Iop.Admin.GeocatClient.UnitTests;
+
+public class GeocatClientMappingRegisterTest
+{
+    [Test]
+    public void Mapster_Configuration_IsValid()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            TypeAdapterConfig.GlobalSettings.Scan(typeof(GeocatClientMappingRegister).Assembly);
+            TypeAdapterConfig.GlobalSettings.Compile();
+        });
+    }
+}
---
+++ src/i14y/Bfs.Iop.Admin.OpenDataClient.UnitTests/OpenDataClientMappingRegisterTest.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using NUnit.Framework;
+
+namespace Bfs.Iop.Admin.OpenDataClient.UnitTests;
+
+public class OpenDataClientMappingRegisterTest
+{
+    [Test]
+    public void Automapper_Configuration_IsValid()
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            TypeAdapterConfig.GlobalSettings.Scan(typeof(OpenDataClientMappingRegister).Assembly);
+            TypeAdapterConfig.GlobalSettings.Compile();
+        });
+    }
+}
