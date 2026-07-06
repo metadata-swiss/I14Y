a40d8486f7132d05c737235e8e948053cf5fee35
yujie_ZHENG_BFS <yujie.zheng@bfs.admin.ch>
2026-07-06 14:23:38 +0200

    create api generated in admin ui

---
+++ src/frontend/admin-ui/api-client/lib/BfsIopAdminApiClientSupport.ts
@@ -0,0 +1,21 @@
+import { HttpResponseBase } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {
+  IApiClientOptionsBuilder,
+  IApiClientSupport
+} from './generated/BfsIopAdminApiClient.extensions.g';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BfsIopAdminApiClientSupport implements IApiClientSupport {
+  SetupApiClientOptions(optionsBuilder: IApiClientOptionsBuilder): void {
+    optionsBuilder.addPrepareRequest(requestOptions => {
+      requestOptions.headers = requestOptions.headers.append(
+        'x-generated-client',
+        'true'
+      );
+    });
+    optionsBuilder.addProcessResponse((response: HttpResponseBase) => {});
+  }
+}
