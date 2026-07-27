7a026a41edd060ca2bf2b3beec1579ab0fa798af
yujie_ZHENG_BFS <yujie.zheng@bfs.admin.ch>
2026-07-27 11:08:46 +0200

    add unitest

--- backend/src/Core/Bfs.Iop.Core.LinkedData.UnitTests/ShaclSparqlQueryHelperTests.cs
+++ backend/src/Core/Bfs.Iop.Core.LinkedData.UnitTests/ShaclSparqlQueryHelperTests.cs
@@ -8,6 +8,7 @@ namespace Bfs.Iop.Core.LinkedData.UnitTests;
 internal sealed class ShaclSparqlQueryHelperTests
 {
     private static readonly SparqlQueryParser Parser = new();
+    private static readonly SparqlUpdateParser UpdateParser = new();
 
     [Test]
     public void Given_valid_concept_iri_When_building_count_query_Then_query_is_parseable_and_contains_the_iri()
@@ -40,5 +41,20 @@ internal sealed class ShaclSparqlQueryHelperTests
 
         Parser.Invoking(p => p.ParseFromString(query)).Should().NotThrow();
         query.Should().NotContain("not-an-absolute-uri");
+    }
+
+    [Test]
+    public void Given_dataset_and_property_uri_When_building_delete_property_update_Then_update_is_parseable_and_guarded()
+    {
+        var datasetId = Guid.Parse("18954b84-d65e-45eb-a6be-b73528f8a7c3");
+        var propertyUri = new Uri("https://example.org/i14y/dataset/ds-1/structure/class-a/property-a");
+
+        var update = ShaclSparqlQueryHelper.DeleteSchemaPropertyQuery(datasetId, propertyUri);
+
+        UpdateParser.Invoking(p => p.ParseFromString(update)).Should().NotThrow();
+        update.Should().Contain($"?root schema:identifier \"{datasetId}\" .");
+        update.Should().Contain($"<{propertyUri}> ?p ?o .");
+        update.Should().Contain($"?parent sh:property <{propertyUri}> .");
+        update.Should().Contain("?list rdf:rest* ?listNode .");
     }
 }
