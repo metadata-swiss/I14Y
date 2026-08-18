2e5d4e7bd255f56e37bd1681ef0a6f4b0ad60dda
yujie_ZHENG_BFS <yujie.zheng@bfs.admin.ch>
2026-08-18 10:59:54 +0200

    update generate files

--- backend/src/Core/Bfs.Iop.Core.ApiClient/Generated/IopCoreApiClient.g.cs
+++ backend/src/Core/Bfs.Iop.Core.ApiClient/Generated/IopCoreApiClient.g.cs
@@ -3170,6 +3170,16 @@ namespace Bfs.Iop.Core.ApiClient
                             throw new ApiException<ProblemDetails>("Bad Request", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
                         }
                         else
+                        if (status_ == 409)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<ProblemDetails>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new ApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            throw new ApiException<ProblemDetails>("Conflict", status_, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
                         if (status_ == 500)
                         {
                             string responseText_ = ( response_.Content == null ) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
