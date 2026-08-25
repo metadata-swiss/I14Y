86847fa69e5b990796a2f64ccb21f6653ce8a23d
Nuno Canadas <nuno.canadas@bfs.admin.ch>
2026-08-25 16:26:48 +0200

    adjust the way that the user is registered

---
+++ backend/src/Admin/Bfs.Iop.Admin.Api/Middleware/RegisterUserMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Bfs.Iop.Core.ApiClient;
+using Bfs.Iop.Infrastructure.ApiClient;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Bfs.Iop.Admin.Api.Middleware;
+
+internal sealed class RegisterUserMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RegisterUserMiddleware> _logger;
+
+    public RegisterUserMiddleware(RequestDelegate next, ILogger<RegisterUserMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context, IIopCoreApiClient apiClient)
+    {
+        var authResult = context.Features.Get<IAuthenticateResultFeature>()?.AuthenticateResult;
+
+        if (authResult?.Succeeded ?? false)
+        {
+            try
+            {
+                await apiClient.GetPersonsSelfRegisteredAsync(default);
+            }
+            catch (ApiException ex)
+            {
+                _logger.LogError(ex, "Error occurred while registering/updating logged user.");
+            }
+        }
+
+        await _next(context);
+    }
+}
