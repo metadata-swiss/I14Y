2a0c597f1ee75d49fc2207563449fee6a62ecbdd
Nuno Canadas <nuno.canadas@bfs.admin.ch>
2026-09-04 12:10:02 +0200

    remove useless files

--- backend/src/Core/Bfs.Iop.Core.Data/Contracts/IDcatCatalogRecordsService.cs
+++
@@ -1,6 +0,0 @@
-﻿using Bfs.Iop.Core.Abstractions.Models;
-
-namespace Bfs.Iop.Core.Data.Contracts;
-
-public interface IDcatCatalogRecordsService : IAuthorizedEntityService
-{ }
--- backend/src/Core/Bfs.Iop.Core.Data/Contracts/IIopDatabaseMigrator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bfs.Iop.Core.Data.Contracts;
-
-public interface IIopDatabaseMigrator
-{
-    Task MigrateAsync(CancellationToken cancellationToken);
-
-    Task InsertSamplesAsync(CancellationToken cancellationToken);
-}
