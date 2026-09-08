edf1c218118abb56224c1b345c75bbe4b5c3ab98
Nuno Canadas <nuno.canadas@bfs.admin.ch>
2026-09-08 08:48:57 +0200

    expose Swagger only for development and dev environment

--- backend/src/AuditTrail/Bfs.Iop.AuditTrail.Api/Program.cs
+++ backend/src/AuditTrail/Bfs.Iop.AuditTrail.Api/Program.cs
@@ -43,7 +43,7 @@ public class Program
 
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("DEV"))
         {
             app.MapOpenApi();
             app.UseSwagger();
