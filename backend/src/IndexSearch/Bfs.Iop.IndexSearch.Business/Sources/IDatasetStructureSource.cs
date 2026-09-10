0b5821b6213cec1e5726ec8b8c283e126c7ef5ea
Mihai <mihai.bercuci@bit.admin.ch>
2026-09-10 09:49:48 +0200

    Feature/863 index search business refinments contracts engine (#194)

--- backend/src/IndexSearch/Bfs.Iop.IndexSearch.Business/Sources/IDatasetStructureSource.cs
+++
@@ -1,6 +0,0 @@
-namespace Bfs.Iop.IndexSearch.Business.Sources;
-
-public interface IDatasetStructureSource
-{
-    Task<IReadOnlySet<Guid>?> GetIdsWithStructuresAsync(CancellationToken cancellationToken = default);
-}
