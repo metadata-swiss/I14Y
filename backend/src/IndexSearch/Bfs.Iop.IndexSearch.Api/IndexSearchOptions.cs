b49822f07aa2ffb1613a06e69f28c4fa0287a51b
Mihai Bercuci <mihai.bercuci@bit.admin.ch>
2026-09-15 15:41:01 +0200

    Simplify StartupAction

--- backend/src/IndexSearch/Bfs.Iop.IndexSearch.Api/IndexSearchOptions.cs
+++ backend/src/IndexSearch/Bfs.Iop.IndexSearch.Api/IndexSearchOptions.cs
@@ -3,7 +3,5 @@ namespace Bfs.Iop.IndexSearch.Api;
 public sealed class IndexSearchOptions
 {
     public const string SectionName = "IndexSearch";
-    public StartupAction OnStartup { get; set; } = StartupAction.CreateIndices;
     public int ReindexBatchSize { get; set; } = 1000;
-    public bool ForceMergeAfterReindex { get; set; } = true;
 }
