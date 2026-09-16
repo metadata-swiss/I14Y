7772f85036db3d6b15d6f16bd1c69732dae7a5e2
Mihai Bercuci <mihai.bercuci@bit.admin.ch>
2026-09-16 14:03:09 +0200

    Move ReindexCounts to its own file

--- backend/src/IndexSearch/Bfs.Iop.IndexSearch.Api/Controllers/IndexStatusResponse.cs
+++ backend/src/IndexSearch/Bfs.Iop.IndexSearch.Api/Controllers/IndexStatusResponse.cs
@@ -1,22 +1,4 @@
-﻿using Bfs.Iop.IndexSearch.Business;
-
-namespace Bfs.Iop.IndexSearch.Api.Controllers;
-
-public sealed record ReindexCounts(
-    int DocumentsSent,
-    int DocumentsWritten,
-    int BatchesFailed,
-    bool? StructuresResolved)
-{
-    internal static ReindexCounts? From(IndexRebuildReport? report) => report is null
-        ? null
-        : new ReindexCounts(
-            report.DocumentsSent,
-            report.DocumentsWritten,
-            report.BatchesFailed,
-            report.StructuresResolved);
-}
-
+﻿namespace Bfs.Iop.IndexSearch.Api.Controllers;
 
 public sealed record IndexStatusResponse(
     bool Running,
